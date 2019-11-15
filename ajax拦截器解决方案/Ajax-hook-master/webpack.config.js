122cd5555b411aeb3f36024f0f4b2dd7c57e03f6
vannvan <862670198@qq.com>
2019-11-15 15:19:40 +0800

    2019/11/15 15:19:39

---
+++ ajax拦截器解决方案/Ajax-hook-master/webpack.config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by du on 16/9/24.
+ */
+var path = require('path');
+var fs = require("fs");
+var webpack = require('webpack');
+module.exports = {
+    entry: {
+        // "ajaxhook": "./src/ajaxhook.js",
+        "ajaxhook": "./dist.js",
+        "test" :["./demon/test.js"],
+        "test1": ["./demon/test1.js"]
+    },
+    output: {
+        path: "./dist",
+        filename: "[name].min.js"
+    },
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: true
+            }
+        }),
+    ]
+}
+
+
